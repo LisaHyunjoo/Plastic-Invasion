d612e45ae0ab6a2144b3acdd6368d92beb298dad
Lisa <lisahongofficial@gmail.com>
2022-07-16 16:09:27 -0400

    game board

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('Hello')
+const gameBoard = document.querySelector(".game-board > ul")
+
+for (let i=0; i<20; i++) {
+    const rows = document.createElement('li')
+    const cols = document.createElement('ul')
+    for (let j=0; j<10; j++) {
+        const cube = document.createElement('li')
+        cols.appendChild(cube)
+    }
+    rows.appendChild(cols)
+    gameBoard.appendChild(rows)
+}
+
+console.log(gameBoard)
