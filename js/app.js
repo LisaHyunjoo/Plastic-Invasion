cca7b54a38f0b633dbf17f50db61296cbd292c40
Lisa <lisahongofficial@gmail.com>
2022-07-16 16:28:29 -0400

    variable of blocks

--- js/app.js
+++ js/app.js
@@ -1,14 +1,29 @@
 const gameBoard = document.querySelector(".game-board > ul")
 
-for (let i=0; i<20; i++) {
-    const rows = document.createElement('li')
-    const cols = document.createElement('ul')
-    for (let j=0; j<10; j++) {
-        const cube = document.createElement('li')
-        cols.appendChild(cube)
-    }
-    rows.appendChild(cols)
-    gameBoard.appendChild(rows)
+const gameRows = 20;
+const gameCols = 10;
+
+const blocks = {
+            //change the block's shape in 4 ways with arrow keys
+    square: [ //represent coordinates (values of x & y)
+        [[0,0], [0,1], [1,1], [1,1]], 
+        [[], [], [], []],
+        [[], [], [], []],
+        [[], [], [], []],
+    ]
 }
 
-console.log(gameBoard)
+function createBoard () {
+    for (let i=0; i<gameRows; i++) {
+        const rows = document.createElement('li')
+        const cols = document.createElement('ul')
+        for (let j=0; j<gameCols; j++) {
+            const cube = document.createElement('li')
+            cols.appendChild(cube)
+        }
+        rows.appendChild(cols)
+        gameBoard.appendChild(rows)
+    }
+}
+createBoard()
+// console.log(gameBoard)
