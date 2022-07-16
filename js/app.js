99cc34ab39949885bd672cb8e5d768b3dc5d152a
Lisa <lisahongofficial@gmail.com>
2022-07-16 18:07:41 -0400

    render a block

--- js/app.js
+++ js/app.js
@@ -5,8 +5,8 @@ const gameCols = 10;
 
 const blocks = {
             //change the block's shape in 4 ways with arrow keys
-    square: [ //represent coordinates (values of x & y)
-        [[0,0], [0,1], [1,1], [1,1]], 
+    tShape: [ //represent coordinates (values of x & y)
+        [[0,1], [1,0], [1,1], [2,1]], 
         [[], [], [], []],
         [[], [], [], []],
         [[], [], [], []],
@@ -18,10 +18,22 @@ let initialBlock;
 
 //actual value of block(type, coordinate)
 const movingBlock = {
-    type:square,
+    type: "tShape",
     direction:0,
     top:0,
     left:0
+}
+
+init()
+
+function init() {
+    //Separate the initial value of block from the value of moving block
+    initialBlock = {...movingBlock}
+    movingBlock.left=3
+    // console.log(initialBlock)
+    // console.log(movingBlock)
+
+    createBoard()
 }
 
 function createBoard () {
@@ -36,5 +48,28 @@ function createBoard () {
         gameBoard.appendChild(rows)
     }
 }
-createBoard()
-// console.log(gameBoard)
+
+function renderBlocks() {
+    //take the initial value of a block
+    const {type, direction, top, left} = initialBlock;
+    // console.log(type, direction, top, left )
+
+    //take a block's type and coordinates of direction
+    // console.log(blocks[type][direction])
+
+    //iterate the block's type and coordinates 
+    blocks[type][direction].forEach(block => {
+        //first element in the array
+        const x = block[0]
+        //second element in the array
+        const y = block[1]
+        
+        //Create the target inside the gameBoard object using coordinates(childnodes)
+        // console.log({gameBoard}) 
+        const target = gameBoard.childNodes[y].childNodes[0].childNodes[x]
+        console.log(target)
+        //add a class to target
+        target.classList.add(type)
+    })  
+}
+renderBlocks()
